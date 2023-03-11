83d5bd518d07e8cac3dcb546deb354962e24b7d6
Vladyslav Ostroushko <v.ostroushko@zajno.com>
2023-03-11 20:39:36 +0200

    Added player, coins, enemies textures. Animated player texute. Added points counter

--- main.py
+++ main.py
@@ -1,58 +1,77 @@
 import pygame
 from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT, K_SPACE
-import numpy as np
+from os import listdir
 import random
 
-screen = w, h = 1440, 768
 is_working = True
 
-BG_COLOR = (0, 0, 0)
-BALL_COLOR = (134, 231, 222)
-ENEMY_COLOR = (255, 0, 0)
-COIN_COLOR = (229, 186, 14)
+BG_IMG = './assets/background.png'
+PLAYER_IMG = './assets/player.png'
+COIN_IMG = './assets/bonus.png'
+ENEMY_IMG = './assets/enemy.png'
+PLAYER_IMGS = './assets/goose'
 
-BALL_SIZE = (50, 50)
-ENEMY_SIZE = (40, 40)
-COIN_SIZE = (25, 25)
+SCORE_COLOR = (0, 0, 0)
 
-ball_speed = 5
+COIN_SIZE = (90, 150)
+ENEMY_SIZE = (153, 54)
+BG_SPEED = 3
+player_speed = 5
 ENEMY_SPEED_RANGE = (5, 10)
 COIN_SPEED_RANGE = (2, 4)
 
-ENEMY_PERIOD = 500
+CHANGE_PLAYER_FRAME_PERIOD = 125
+ENEMY_PERIOD = 1500
 COIN_PERIOD = 5000
+
+SCORE_TEXT = 'POINTS: '
+
+player_frames_index = 0
+player_score = 0
+enemies = []
+coins = []
 
 pygame.init()
 
+display_info = pygame.display.Info()
+screen = w, h = display_info.current_w, display_info.current_h
+
 FPS = pygame.time.Clock()
+
 CREATE_ENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_ENEMY, ENEMY_PERIOD)
 
 CREATE_COIN = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_COIN, COIN_PERIOD)
 
+CHANGE_PLAYER_FRAME = pygame.USEREVENT + 3
+pygame.time.set_timer(CHANGE_PLAYER_FRAME, CHANGE_PLAYER_FRAME_PERIOD)
+
 main_surface = pygame.display.set_mode(screen)
 
-ball = pygame.Surface(BALL_SIZE)
-ball.fill(BALL_COLOR)
-ball_rect = ball.get_rect()
+font = pygame.font.SysFont('Verdana', 40)
+
+bg = pygame.transform.scale(pygame.image.load(BG_IMG).convert(), screen)
+
+bgX = 0
+bgX2 = bg.get_width()
+
+player_frames = [pygame.image.load(PLAYER_IMGS + '/' + file).convert_alpha() for file in listdir(PLAYER_IMGS)]
+player = player_frames[player_frames_index]
+player_rect = player.get_rect()
 
 def create_enemy():
-    enemy = pygame.Surface(ENEMY_SIZE)
-    enemy.fill(ENEMY_COLOR)
-    enemy_rect = pygame.Rect(w, random.randint(0, h), *enemy.get_size())
+    enemy = pygame.transform.scale(pygame.image.load(ENEMY_IMG).convert_alpha(), ENEMY_SIZE)
+    enemy_rect = pygame.Rect(w + enemy.get_width(), random.randint(0, h), *enemy.get_size())
     enemy_speed = random.randint(ENEMY_SPEED_RANGE[0], ENEMY_SPEED_RANGE[1])
     return [enemy, enemy_rect, enemy_speed]
 
 def create_coin():
-    coin = pygame.Surface(COIN_SIZE)
-    coin.fill(COIN_COLOR)
-    coin_rect = pygame.Rect(random.randint(0, w), 0, *coin.get_size())
+    coin = pygame.transform.scale(pygame.image.load(COIN_IMG).convert_alpha(), COIN_SIZE)
+    coin_rect = pygame.Rect(random.randint(0, w), 0 - coin.get_height(), *coin.get_size())
     coin_speed = random.randint(COIN_SPEED_RANGE[0], COIN_SPEED_RANGE[1])
     return [coin, coin_rect, coin_speed]
 
-enemies = []
-coins = []
 
 while is_working:
     FPS.tick(60)
@@ -66,13 +85,32 @@ while is_working:
         
         if event.type == CREATE_COIN:
             coins.append(create_coin())
+        
+        if event.type == CHANGE_PLAYER_FRAME:
+            player_frames_index += 1
+            if player_frames_index == len(player_frames):
+                player_frames_index = 0
+            player = player_frames[player_frames_index]
 
     pressed_key = pygame.key.get_pressed()
 
-    main_surface.fill(BG_COLOR)
+    bgX -= BG_SPEED
+    bgX2 -= BG_SPEED
 
-    main_surface.blit(ball, ball_rect)
+    main_surface.blit(bg, (bgX, 0))
+    main_surface.blit(bg, (bgX2, 0))
 
+    if bgX < -bg.get_width():
+        bgX = bg.get_width()
+
+    if bgX2 < -bg.get_width():
+        bgX2 = bg.get_width()  
+
+    main_surface.blit(player, player_rect)
+
+    rendered_text = font.render(SCORE_TEXT + str(player_score), True, SCORE_COLOR)
+
+    main_surface.blit(rendered_text, (w - rendered_text.get_width(), 0))
     for enemy in enemies:
         main_surface.blit(enemy[0], enemy[1])
         enemy[1] = enemy[1].move(-enemy[2], 0)
@@ -80,8 +118,8 @@ while is_working:
         if enemy[1].left < 0:
             enemies.pop(enemies.index(enemy))
         
-        if ball_rect.colliderect(enemy[1]):
-            enemies.pop(enemies.index(enemy))
+        if player_rect.colliderect(enemy[1]):
+            is_working = False
 
     for coin in coins:
         main_surface.blit(coin[0], coin[1])
@@ -90,28 +128,20 @@ while is_working:
         if coin[1].bottom > h:
             coins.pop(coins.index(coin))
         
-        if ball_rect.colliderect(coin[1]):
+        if player_rect.colliderect(coin[1]):
             coins.pop(coins.index(coin))
+            player_score += 1
 
-    if pressed_key[K_DOWN] and ball_rect.bottom <= h:
-        ball_rect = ball_rect.move(0, ball_speed)
+    if pressed_key[K_DOWN] and player_rect.bottom <= h:
+        player_rect = player_rect.move(0, player_speed)
 
-    if pressed_key[K_UP] and ball_rect.top >= 0:
-        ball_rect = ball_rect.move(0, -ball_speed)
+    if pressed_key[K_UP] and player_rect.top >= 0:
+        player_rect = player_rect.move(0, -player_speed)
     
-    if pressed_key[K_RIGHT] and ball_rect.right <= w:
-        ball_rect = ball_rect.move(ball_speed, 0)
+    if pressed_key[K_RIGHT] and player_rect.right <= w:
+        player_rect = player_rect.move(player_speed, 0)
 
-    if pressed_key[K_LEFT] and ball_rect.left >= 0:
-        ball_rect = ball_rect.move(-ball_speed, 0)
+    if pressed_key[K_LEFT] and player_rect.left >= 0:
+        player_rect = player_rect.move(-player_speed, 0)
 
     pygame.display.flip()
-
-
-# if ball_rect.bottom >= h or ball_rect.top <= 0:
-#     ball_speed[1]= -ball_speed[1]
-#     ball.fill(list(np.random.choice(range(256), size=3)))
-    
-# if ball_rect.right >= w or ball_rect.left <= 0:
-#     ball_speed[0]= -ball_speed[0]
-#     ball.fill(list(np.random.choice(range(256), size=3)))
